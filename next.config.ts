839723b8a8199deabef8bc24128109da74208170
AnimeshA867 <animeshacharya867+github@gmail.com>
2026-03-05 23:51:52 +0545

    chore: Fixed turbopack error

--- next.config.ts
+++ next.config.ts
@@ -6,18 +6,14 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true, // Internal navigation fast garauna
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swMinify: true, // File size sano banauna
   disable: process.env.NODE_ENV === "development", // Dev mode ma error naaos bhannalai
   workboxOptions: {
-    expiration: {
-      maxEntries: 50,
-      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days cache
-    },
   },
 });
 
 export default withPWA({
   // Tapaiko aru normal Next.js configs yaha hunchhan
   reactStrictMode: true,
+  turbopack: {},
 });
 
